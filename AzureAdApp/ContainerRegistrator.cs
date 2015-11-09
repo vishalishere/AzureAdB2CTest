4960b30d55c8de4b3bb8d16cd3d79b13a449221c
Alexander Viken <xadhoom@msn.com>
2015-11-09 11:03:00 +0100

    Added setup for Prism and Unity

---
+++ AzureAdApp/ContainerRegistrator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace AzureAdApp
+{
+    public static class ContainerRegistrator
+    {
+        private static IUnityContainer _container;
+
+        public static void RegisterContainers(IUnityContainer container)
+        {
+            _container = container;
+            RegisterSubscription();
+            RegisterResourceGallery();
+        }
+
+        private static void RegisterResourceGallery()
+        {
+            //_container
+            //    .RegisterType<IResourceTemplateGalleryManager, ResourceTemplateGalleryManager>(
+            //            new ContainerControlledLifetimeManager());
+            //_container
+            //    .RegisterType<IResourceTemplateGalleryRepository, ResourceTemplateGalleryRepository>(
+            //        new ContainerControlledLifetimeManager());
+
+        }
+        private static void RegisterSubscription()
+        {
+        //    _container
+        //        .RegisterType<ISubscriptionManager, SubscriptionManager>(
+        //        new ContainerControlledLifetimeManager());
+        //    _container
+        //        .RegisterType<ISubscriptionRepository, SubscriptionRepository>(
+        //            new ContainerControlledLifetimeManager());
+        //    _container
+        //        .RegisterType<ISubscriptionFactory, SubscriptionFactory>(
+        //            new ContainerControlledLifetimeManager());
+        }
+
+    }
+}
---
+++ AzureAdApp/PageBase/PrismPage.cs
@@ -0,0 +1,9 @@
+﻿using Windows.UI.Xaml.Controls;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace AzureAdApp.PageBase
+{
+    public abstract partial class PrismPage : Page, IView
+    {
+    }
+}
